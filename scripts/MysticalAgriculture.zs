EnigmaticaModpacks/Enigmatica2Expert

commit 441a0348cd408f7c134fe6944dad511e3acaf892

Unified Sulfur, closes #860

scripts/MysticalAgriculture.zs
@@ -642,5 +642,10 @@ recipes.addShapedMirrored("Wither Proof Glass",
 	rh(<mysticalagriculture:inferium_ore>);
 	rh(<mysticalagriculture:nether_inferium_ore>);
 	rh(<mysticalagriculture:end_inferium_ore>);
+
+# *======= Unification =======*
+
+  recipes.removeByRecipeName("mysticalagriculture:dustsulfur");
+	recipes.addShapeless("Sulfur Essence to Dust", <thermalfoundation:material:771> * 8, [<mysticalagriculture:sulfur_essence>,<mysticalagriculture:sulfur_essence>,<mysticalagriculture:sulfur_essence>]);
 	
 	print("--- MysticalAgriculture.zs initialized ---");

scripts/ImmersiveEngineering.zs
@@ -23,6 +23,10 @@ print("--- loading ImmersiveEngineering.zs ---");
 	mods.thermalexpansion.RedstoneFurnace.addPyrolysisRecipe(<immersiveengineering:stone_decoration:3>, 
 	<minecraft:coal_block>, 2400, 2500);
 
+# Sulfur Unification
+  mods.immersiveengineering.Crusher.removeRecipe(<minecraft:blaze_powder>);
+  mods.immersiveengineering.Crusher.addRecipe(<minecraft:blaze_powder> * 4, <ore:itemBlazeRod>, 2048, <thermalfoundation:material:771>, 0.5);
+
 # Aluminum Scaffolding
 	recipes.remove(<immersiveengineering:metal_decoration2:8>);
 	recipes.addShapedMirrored("Aluminum Scaffolding", 

commit 3449505fe88d16f97f7d452edeebb29bdc6d2b31

Readded Sulfur Unification from 441a034
Accidentally removed it when reverting changes, whoops!

scripts/MysticalAgriculture.zs
@@ -657,4 +657,10 @@ recipes.addShapedMirrored("Wither Proof Glass",
 	rh(<mysticalagriculture:nether_inferium_ore>);
 	rh(<mysticalagriculture:end_inferium_ore>);
 	
+# *======= Unification =======*
+
+# Sulfur Essence to Dust
+  recipes.removeByRecipeName("mysticalagriculture:dustsulfur");
+  recipes.addShapeless("mysticalagriculture:dustsulfur", <thermalfoundation:material:771> * 8, [<mysticalagriculture:sulfur_essence>,<mysticalagriculture:sulfur_essence>,<mysticalagriculture:sulfur_essence>]);
+
 	print("--- MysticalAgriculture.zs initialized ---");