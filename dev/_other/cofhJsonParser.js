196a66d7608a899e5328acba5a31af610201ae44
Krutoy242 <Krutoy242@gmail.com>
2020-12-10 21:17:51 +0300

    🪐 123 Kepler 0119 tweaks: - Huge caverns - Fake Iron & Oil lakes - Stalagmites of resources

--- dev/_other/cofhJsonParser.js
+++ dev/_other/cofhJsonParser.js
@@ -4,15 +4,20 @@ Making batch changing inside JSON files of COFH-World
 
 const replace = require('replace-in-file')
 
+console.log('Files changed:')
+console.log(
 replace.sync({
   files: 'config/cofh/world/*.json',
   from: /("dimension".*?\n?.*?"restriction".*?\n?.*?"blacklist".*?\n?.*?"value"[^\]]+?)(\[[^\]]+?\])/gm,
   to: (...args) => {
     const captureArr = new Set(eval(`${args[2]}`))
-    captureArr.add(111)
+    captureArr.add(123)
     const arr = JSON.stringify([...captureArr].sort())
-    console.log('arr :>> ', arr, args.pop())
+    // console.log('arr :>> ', arr, args.pop())
     return `${args[1]}${arr}`
   },
   // dry: true
 })
+.filter(r=>r.hasChanged)
+.map(r=>r.file)
+)
