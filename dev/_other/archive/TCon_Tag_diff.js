2a8f14163b16725f1cfccdea20e8bafb5f3b889a
Krutoy242 <Krutoy242@gmail.com>
2021-01-02 16:50:19 +0300

    🧱 Other .JS files refactor

--- dev/_other/archive/TCon_Tag_diff.js
+++ dev/_other/archive/TCon_Tag_diff.js
@@ -1,4 +1,4 @@
-var _ = require("lodash");
+var _ = require('lodash')
 
 /**
  * Deep diff between two object-likes
@@ -7,13 +7,10 @@ var _ = require("lodash");
  * @return {Object}            a new object which represents the diff
  */
 function deepDiff(fromObject, toObject) {
-  const changes = {};
+  const changes = {}
 
   const buildPath = (path, obj, key) =>
-    _.isUndefined(path) ? key : `${path}.${key}`;
-
-  const arrHave = (o, v) =>
-    Array.isArray(o) && o.find((oo) => _.isEqual(oo, v));
+    _.isUndefined(path) ? key : `${path}.${key}`
 
   const walk = (fromObject, toObject, path) => {
     // if (Array.isArray(fromObject)) fromObject.sort();
@@ -22,82 +19,82 @@ function deepDiff(fromObject, toObject) {
     if (Array.isArray(fromObject) && Array.isArray(toObject)) {
       var duplicates = fromObject.filter((val) =>
         toObject.find((o) => o != val)
-      );
+      )
       // console.log('duplicates :>> ', duplicates);
       fromObject = fromObject.filter(
         (val) => !duplicates.find((o) => _.isEqual(o, val))
-      );
+      )
       toObject = toObject.filter(
         (val) => !duplicates.find((o) => _.isEqual(o, val))
-      );
+      )
       // console.log('fromObject :>> ', fromObject);
       // console.log('toObject :>> ', toObject);
     }
 
     for (const key of _.keys(fromObject)) {
-      const currentPath = buildPath(path, fromObject, key);
+      const currentPath = buildPath(path, fromObject, key)
       if (!_.has(toObject, key)) {
-        changes[currentPath] = { from: _.get(fromObject, key) };
+        changes[currentPath] = { from: _.get(fromObject, key) }
       }
     }
 
     for (const [key, to] of _.entries(toObject)) {
-      const currentPath = buildPath(path, toObject, key);
+      const currentPath = buildPath(path, toObject, key)
       if (!_.has(fromObject, key)) {
-        changes[currentPath] = { to };
+        changes[currentPath] = { to }
       } else {
-        const from = _.get(fromObject, key);
+        const from = _.get(fromObject, key)
         if (!_.isEqual(from, to)) {
           if (_.isObjectLike(to) && _.isObjectLike(from)) {
-            walk(from, to, currentPath);
+            walk(from, to, currentPath)
           } else {
-            changes[currentPath] = { from, to };
+            changes[currentPath] = { from, to }
           }
         }
       }
     }
-  };
+  }
 
-  walk(fromObject, toObject);
+  walk(fromObject, toObject)
 
-  return changes;
+  return changes
 }
 
 // mom I'm on lodash
-_.mixin({ deepDiff });
+_.mixin({ deepDiff })
 
-var rootTag;
+var rootTag
 function withTag(tag) {
-  rootTag = rootTag || tag;
+  rootTag = rootTag || tag
 
-  var diff = deepDiff(rootTag, tag);
+  var diff = deepDiff(rootTag, tag)
 
   // console.log('diff :>> ', diff);
-  var modifier = ((diff["Modifiers.0"] || {}).to || {}).identifier;
-  console.log(`if (modifier == "${modifier}") {`);
+  var modifier = ((diff['Modifiers.0'] || {}).to || {}).identifier
+  console.log(`if (modifier == "${modifier}") {`)
   for (const [k, v] of _.entries(diff)) {
 
-    var s = "";
-    var keys = `tag.${k}`;
+    var s = ''
+    var keys = `tag.${k}`
     if (v.from && v.to) {
-      s = `(${keys}.asFloat() + ${v.to - v.from}f)`;
+      s = `(${keys}.asFloat() + ${v.to - v.from}f)`
     } else if (v.to && k.match(/^(.*?)\.\d+$/)) {
       keys = `tag.${k.match(/^(.*?)\.\d+$/)[1]}`
-      s = `${keys} + [${JSON.stringify(v.to)}]`;
+      s = `${keys} + [${JSON.stringify(v.to)}]`
     } else {
-      s = `${JSON.stringify(v.to)}`;
+      s = `${JSON.stringify(v.to)}`
     }
-    console.log(`  ${keys.padEnd(30)} = ${s} as IData;`);
+    console.log(`  ${keys.padEnd(30)} = ${s} as IData;`)
   }
-  console.log("}");
+  console.log('}')
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-withTag({StatsOriginal: {Toughness: 0.0, FreeModifiers: 3, Durability: 64, Defense: 3.0}, Stats: {Toughness: 0.0, FreeModifiers: 3, Durability: 64, Defense: 3.0}, TinkerData: {Materials: ["wood", "wood", "wood"], Modifiers: []}, Modifiers: [{identifier: "ecological_armor", color: -7444965, level: 1}], Traits: ["ecological_armor"]})
-withTag({ench: [{lvl: 4, id: 21}, {lvl: 4, id: 35}], StatsOriginal: {AttackSpeedMultiplier: 1.0, MiningSpeed: 2.255, FreeModifiers: 3, Durability: 2, HarvestLevel: 1, Attack: 4.9825}, Stats: {AttackSpeedMultiplier: 1.0, MiningSpeed: 2.255, FreeModifiers: 0, Durability: 1, HarvestLevel: 1, Attack: 16.9825}, Special: {Categories: ["tool", "weapon"]}, TinkerData: {UsedModifiers: 12, Materials: ["stone", "paper", "stone", "stone"], Modifiers: ["beheading_cleaver", "sharpness", "luck", "beheading"]}, Traits: ["cheap", "writable2", "cheapskate", "luck"], Modifiers: [{identifier: "cheap", color: -6710887, level: 1}, {identifier: "writable", color: -1, level: 1, writable2: 1}, {identifier: "cheapskate", color: -6710887, level: 1}, {identifier: "beheading_cleaver", color: 1070923, level: 2}, {identifier: "sharpness", current: 288, color: 16774902, level: 4, max: 72, extraInfo: "287 / 72"}, {identifier: "luck", current: 240, color: 2970082, level: 4, max: 60, modifierUsed: 1, extraInfo: "239 / 60"}, {identifier: "beheading", color: 1070923, level: 4}]})
-withTag({ench: [{lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}], StatsOriginal: {AttackSpeedMultiplier: 1.0, MiningSpeed: 3.05, FreeModifiers: 3, Durability: 484, HarvestLevel: 1, Attack: 9.5}, Stats: {AttackSpeedMultiplier: 1.0, MiningSpeed: 3.05, FreeModifiers: 0, Durability: 387, HarvestLevel: 1, Attack: 21.5}, Special: {Categories: ["weapon", "tool"]}, TinkerData: {UsedModifiers: 12, Materials: ["obsidian", "magmaslime", "stone", "stone"], Modifiers: ["beheading_cleaver", "sharpness", "luck", "beheading"]}, Traits: ["duritos", "superheat", "cheapskate", "cheap", "luck"], Modifiers: [{identifier: "duritos", color: -10478396, level: 1}, {identifier: "superheat", color: -27123, level: 1}, {identifier: "cheapskate", color: -6710887, level: 1}, {identifier: "cheap", color: -6710887, level: 1}, {identifier: "beheading_cleaver", color: 1070923, level: 2}, {identifier: "sharpness", current: 288, color: 16774902, level: 4, max: 72, extraInfo: "287 / 72"}, {identifier: "luck", current: 240, color: 2970082, level: 4, max: 60, modifierUsed: 1, extraInfo: "239 / 60"}, {identifier: "beheading", color: 1070923, level: 4}]})
-withTag({ench: [{lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}], StatsOriginal: {AttackSpeedMultiplier: 1.0, MiningSpeed: 4.5, FreeModifiers: 3, Durability: 218, HarvestLevel: 1, Attack: 6.835}, Stats: {AttackSpeedMultiplier: 1.0, MiningSpeed: 4.5, FreeModifiers: 0, Durability: 174, HarvestLevel: 1, Attack: 18.835}, Special: {Categories: ["weapon", "tool"]}, TinkerData: {UsedModifiers: 12, Materials: ["slime", "stone", "flint", "stone"], Modifiers: ["beheading_cleaver", "sharpness", "luck", "beheading"]}, Traits: ["slimey_green", "cheapskate", "crude2", "cheap", "luck"], Modifiers: [{identifier: "slimey_green", color: -8206221, level: 1}, {identifier: "cheapskate", color: -6710887, level: 1}, {identifier: "crude", color: -9868951, level: 2, crude2: 1}, {identifier: "cheap", color: -6710887, level: 1}, {identifier: "beheading_cleaver", color: 1070923, level: 2}, {identifier: "sharpness", current: 288, color: 16774902, level: 4, max: 72, extraInfo: "287 / 72"}, {identifier: "luck", current: 240, color: 2970082, level: 4, max: 60, modifierUsed: 1, extraInfo: "239 / 60"}, {identifier: "beheading", color: 1070923, level: 4}]})
-withTag({StatsOriginal: {Toughness: 0.0, FreeModifiers: 3, Durability: 1, Defense: 0.5}, Stats: {Toughness: 0.0, FreeModifiers: 3, Durability: 1, Defense: 0.5}, TinkerData: {UsedModifiers: 2, Materials: ["paper", "stone", "cactus", "paper"], Modifiers: ["creative", "creative", "speedy_armor", "creative", "creative", "speedy_armor", "shulkerweight_armor"]}, Modifiers: [{identifier: "writable", color: -1, level: 1, writable2: 1}, {identifier: "cheap_armor", color: -6710887, level: 1}, {identifier: "spiny_armor", color: -16735985, level: 1}, {identifier: "creative", color: 0, level: 1}, {identifier: "speedy_armor", current: 50, color: 9502720, level: 1, max: 50, extraInfo: "49 / 50"}, {identifier: "shulkerweight_armor", current: 20, color: 11193599, level: 1, max: 20, extraInfo: "19 / 20"}], Traits: ["writable2", "cheap_armor", "spiny_armor", "speedy_armor", "shulkerweight_armor"]})
-withTag({StatsOriginal: {Toughness: 0.0, FreeModifiers: 3, Durability: 402, Defense: 5.3}, Stats: {Toughness: 0.0, FreeModifiers: 2, Durability: 402, Defense: 5.3}, TinkerData: {UsedModifiers: 2, Materials: ["netherrack", "cobalt", "netherrack", "slime"], Modifiers: ["creative", "creative", "projectile_resistant_armor", "creative", "creative", "projectile_resistant_armor", "speedy_armor"]}, Modifiers: [{identifier: "aridiculous_armor", color: -4698289, level: 1}, {identifier: "infernal_armor", color: -4698289, level: 1}, {identifier: "lightweight_armor", color: -14122284, level: 1}, {identifier: "creative", color: 0, level: 1}, {identifier: "projectile_resistant_armor", color: 1070923, level: 1, modifierUsed: 1}, {identifier: "speedy_armor", current: 50, color: 9502720, level: 1, max: 50, extraInfo: "49 / 50"}], Traits: ["aridiculous_armor", "infernal_armor", "lightweight_armor", "projectile_resistant_armor", "speedy_armor"]})
-withTag({StatsOriginal: {Toughness: 0.0, FreeModifiers: 3, Durability: 402, Defense: 5.3}, Stats: {Toughness: 0.0, FreeModifiers: 4, Durability: 402, Defense: 5.3}, TinkerData: {UsedModifiers: 3, Materials: ["netherrack", "cobalt", "netherrack", "slime"], Modifiers: ["creative", "creative", "projectile_resistant_armor", "creative", "creative", "projectile_resistant_armor", "speedy_armor"]}, Modifiers: [{identifier: "aridiculous_armor", color: -4698289, level: 1}, {identifier: "infernal_armor", color: -4698289, level: 1}, {identifier: "lightweight_armor", color: -14122284, level: 1}, {identifier: "creative", color: 0, level: 1}, {identifier: "projectile_resistant_armor", color: 1070923, level: 1, modifierUsed: 1}, {identifier: "speedy_armor", current: 64, color: 9502720, level: 2, max: 100, extraInfo: "63 / 100"}], Traits: ["aridiculous_armor", "infernal_armor", "lightweight_armor", "projectile_resistant_armor", "speedy_armor"]})
+// withTag({StatsOriginal: {Toughness: 0.0, FreeModifiers: 3, Durability: 64, Defense: 3.0}, Stats: {Toughness: 0.0, FreeModifiers: 3, Durability: 64, Defense: 3.0}, TinkerData: {Materials: ["wood", "wood", "wood"], Modifiers: []}, Modifiers: [{identifier: "ecological_armor", color: -7444965, level: 1}], Traits: ["ecological_armor"]})
+// withTag({ench: [{lvl: 4, id: 21}, {lvl: 4, id: 35}], StatsOriginal: {AttackSpeedMultiplier: 1.0, MiningSpeed: 2.255, FreeModifiers: 3, Durability: 2, HarvestLevel: 1, Attack: 4.9825}, Stats: {AttackSpeedMultiplier: 1.0, MiningSpeed: 2.255, FreeModifiers: 0, Durability: 1, HarvestLevel: 1, Attack: 16.9825}, Special: {Categories: ["tool", "weapon"]}, TinkerData: {UsedModifiers: 12, Materials: ["stone", "paper", "stone", "stone"], Modifiers: ["beheading_cleaver", "sharpness", "luck", "beheading"]}, Traits: ["cheap", "writable2", "cheapskate", "luck"], Modifiers: [{identifier: "cheap", color: -6710887, level: 1}, {identifier: "writable", color: -1, level: 1, writable2: 1}, {identifier: "cheapskate", color: -6710887, level: 1}, {identifier: "beheading_cleaver", color: 1070923, level: 2}, {identifier: "sharpness", current: 288, color: 16774902, level: 4, max: 72, extraInfo: "287 / 72"}, {identifier: "luck", current: 240, color: 2970082, level: 4, max: 60, modifierUsed: 1, extraInfo: "239 / 60"}, {identifier: "beheading", color: 1070923, level: 4}]})
+// withTag({ench: [{lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}], StatsOriginal: {AttackSpeedMultiplier: 1.0, MiningSpeed: 3.05, FreeModifiers: 3, Durability: 484, HarvestLevel: 1, Attack: 9.5}, Stats: {AttackSpeedMultiplier: 1.0, MiningSpeed: 3.05, FreeModifiers: 0, Durability: 387, HarvestLevel: 1, Attack: 21.5}, Special: {Categories: ["weapon", "tool"]}, TinkerData: {UsedModifiers: 12, Materials: ["obsidian", "magmaslime", "stone", "stone"], Modifiers: ["beheading_cleaver", "sharpness", "luck", "beheading"]}, Traits: ["duritos", "superheat", "cheapskate", "cheap", "luck"], Modifiers: [{identifier: "duritos", color: -10478396, level: 1}, {identifier: "superheat", color: -27123, level: 1}, {identifier: "cheapskate", color: -6710887, level: 1}, {identifier: "cheap", color: -6710887, level: 1}, {identifier: "beheading_cleaver", color: 1070923, level: 2}, {identifier: "sharpness", current: 288, color: 16774902, level: 4, max: 72, extraInfo: "287 / 72"}, {identifier: "luck", current: 240, color: 2970082, level: 4, max: 60, modifierUsed: 1, extraInfo: "239 / 60"}, {identifier: "beheading", color: 1070923, level: 4}]})
+// withTag({ench: [{lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}, {lvl: 1, id: 21}, {lvl: 1, id: 35}], StatsOriginal: {AttackSpeedMultiplier: 1.0, MiningSpeed: 4.5, FreeModifiers: 3, Durability: 218, HarvestLevel: 1, Attack: 6.835}, Stats: {AttackSpeedMultiplier: 1.0, MiningSpeed: 4.5, FreeModifiers: 0, Durability: 174, HarvestLevel: 1, Attack: 18.835}, Special: {Categories: ["weapon", "tool"]}, TinkerData: {UsedModifiers: 12, Materials: ["slime", "stone", "flint", "stone"], Modifiers: ["beheading_cleaver", "sharpness", "luck", "beheading"]}, Traits: ["slimey_green", "cheapskate", "crude2", "cheap", "luck"], Modifiers: [{identifier: "slimey_green", color: -8206221, level: 1}, {identifier: "cheapskate", color: -6710887, level: 1}, {identifier: "crude", color: -9868951, level: 2, crude2: 1}, {identifier: "cheap", color: -6710887, level: 1}, {identifier: "beheading_cleaver", color: 1070923, level: 2}, {identifier: "sharpness", current: 288, color: 16774902, level: 4, max: 72, extraInfo: "287 / 72"}, {identifier: "luck", current: 240, color: 2970082, level: 4, max: 60, modifierUsed: 1, extraInfo: "239 / 60"}, {identifier: "beheading", color: 1070923, level: 4}]})
+// withTag({StatsOriginal: {Toughness: 0.0, FreeModifiers: 3, Durability: 1, Defense: 0.5}, Stats: {Toughness: 0.0, FreeModifiers: 3, Durability: 1, Defense: 0.5}, TinkerData: {UsedModifiers: 2, Materials: ["paper", "stone", "cactus", "paper"], Modifiers: ["creative", "creative", "speedy_armor", "creative", "creative", "speedy_armor", "shulkerweight_armor"]}, Modifiers: [{identifier: "writable", color: -1, level: 1, writable2: 1}, {identifier: "cheap_armor", color: -6710887, level: 1}, {identifier: "spiny_armor", color: -16735985, level: 1}, {identifier: "creative", color: 0, level: 1}, {identifier: "speedy_armor", current: 50, color: 9502720, level: 1, max: 50, extraInfo: "49 / 50"}, {identifier: "shulkerweight_armor", current: 20, color: 11193599, level: 1, max: 20, extraInfo: "19 / 20"}], Traits: ["writable2", "cheap_armor", "spiny_armor", "speedy_armor", "shulkerweight_armor"]})
+// withTag({StatsOriginal: {Toughness: 0.0, FreeModifiers: 3, Durability: 402, Defense: 5.3}, Stats: {Toughness: 0.0, FreeModifiers: 2, Durability: 402, Defense: 5.3}, TinkerData: {UsedModifiers: 2, Materials: ["netherrack", "cobalt", "netherrack", "slime"], Modifiers: ["creative", "creative", "projectile_resistant_armor", "creative", "creative", "projectile_resistant_armor", "speedy_armor"]}, Modifiers: [{identifier: "aridiculous_armor", color: -4698289, level: 1}, {identifier: "infernal_armor", color: -4698289, level: 1}, {identifier: "lightweight_armor", color: -14122284, level: 1}, {identifier: "creative", color: 0, level: 1}, {identifier: "projectile_resistant_armor", color: 1070923, level: 1, modifierUsed: 1}, {identifier: "speedy_armor", current: 50, color: 9502720, level: 1, max: 50, extraInfo: "49 / 50"}], Traits: ["aridiculous_armor", "infernal_armor", "lightweight_armor", "projectile_resistant_armor", "speedy_armor"]})
+// withTag({StatsOriginal: {Toughness: 0.0, FreeModifiers: 3, Durability: 402, Defense: 5.3}, Stats: {Toughness: 0.0, FreeModifiers: 4, Durability: 402, Defense: 5.3}, TinkerData: {UsedModifiers: 3, Materials: ["netherrack", "cobalt", "netherrack", "slime"], Modifiers: ["creative", "creative", "projectile_resistant_armor", "creative", "creative", "projectile_resistant_armor", "speedy_armor"]}, Modifiers: [{identifier: "aridiculous_armor", color: -4698289, level: 1}, {identifier: "infernal_armor", color: -4698289, level: 1}, {identifier: "lightweight_armor", color: -14122284, level: 1}, {identifier: "creative", color: 0, level: 1}, {identifier: "projectile_resistant_armor", color: 1070923, level: 1, modifierUsed: 1}, {identifier: "speedy_armor", current: 64, color: 9502720, level: 2, max: 100, extraInfo: "63 / 100"}], Traits: ["aridiculous_armor", "infernal_armor", "lightweight_armor", "projectile_resistant_armor", "speedy_armor"]})
--- dev/_other/betterQuesting.js
+++ dev/_other/betterQuesting.js
@@ -2,49 +2,41 @@
 /*=============================================
 =                Variables                    =
 =============================================*/
-const utils = require('../../lib/utils.js');
+const utils = require('../lib/utils.js')
 
 /*=============================================
 =                  Class                      =
 =============================================*/
 class Book {
-  defaultQuests;
-
   constructor(path) {
     this.defaultQuests = utils.loadJson(path)
   }
-  
+
 
   questByID(id) {
-    var quests = this.defaultQuests["questDatabase:9"];
+    var quests = this.defaultQuests['questDatabase:9']
 
     for (const key in quests) {
-      if (quests.hasOwnProperty(key)) {
-        const quest = quests[key];
-        
-        if (quest["questID:3"] === id) return quest;
-      }
+      const quest = quests[key]
+
+      if (quest['questID:3'] === id) return quest
     }
   }
 
 
   forEachQuestInChapter(chapterName, fnc) {
-    var chapters = this.defaultQuests["questLines:9"];
+    var chapters = this.defaultQuests['questLines:9']
     for (const chap in chapters) {
-      if (chapters.hasOwnProperty(chap)) {
-        const chapter = chapters[chap];
-        
-        if (chapter["properties:10"]["betterquesting:10"]["name:8"] === chapterName) {
-          for (const key in chapter["quests:9"]) {
-            if (chapter["quests:9"].hasOwnProperty(key)) {
-              const questLine = chapter["quests:9"][key];
+      const chapter = chapters[chap]
 
-              var quest = this.questByID(questLine["id:3"]);
-              if (quest) fnc(quest);
-            }
-          }
-          return;
+      if (chapter['properties:10']['betterquesting:10']['name:8'] === chapterName) {
+        for (const key in chapter['quests:9']) {
+          const questLine = chapter['quests:9'][key]
+
+          var quest = this.questByID(questLine['id:3'])
+          if (quest) fnc(quest)
         }
+        return
       }
     }
   }
@@ -55,45 +47,45 @@ class Book {
 =============================================*/
 function bq_item(itemName, meta = 0, count = 1) {
   return {
-    "id:8": itemName,
-    "Count:3": count,
-    "Damage:2": meta
-  };
+    'id:8': itemName,
+    'Count:3': count,
+    'Damage:2': meta
+  }
 }
 
 function bq_rewardItem(itemName, meta = 0, count = 1, index = 0) {
   return {
-    "0:10": {
-      "rewardID:8": "bq_standard:item",
-      "index:3": index,
-      "rewards:9": {
-        "0:10": bq_item(itemName, meta, count)
+    '0:10': {
+      'rewardID:8': 'bq_standard:item',
+      'index:3': index,
+      'rewards:9': {
+        '0:10': bq_item(itemName, meta, count)
       }
     }
-  };
+  }
 }
 
 
 function bq_rewardChoice(lootchestLevel, itemName1, meta1=0, count1=1, itemName2, meta2=0, count2=1) {
   var r = {
-    "0:10": {
-      "rewardID:8": "bq_standard:choice",
-      "index:3": 0,
-      "choices:9": {
-        "0:10": {
-          "id:8": "bq_standard:loot_chest",
-          "Count:3": 1,
-          "Damage:2": lootchestLevel
+    '0:10': {
+      'rewardID:8': 'bq_standard:choice',
+      'index:3': 0,
+      'choices:9': {
+        '0:10': {
+          'id:8': 'bq_standard:loot_chest',
+          'Count:3': 1,
+          'Damage:2': lootchestLevel
         },
-        "1:10": bq_item(itemName1, meta1, count1)
+        '1:10': bq_item(itemName1, meta1, count1)
       }
     }
-  };
+  }
   if (itemName2) {
-    r["0:10"]["choices:9"]["2:10"] = bq_item(itemName2, meta2, count2);
+    r['0:10']['choices:9']['2:10'] = bq_item(itemName2, meta2, count2)
   }
 
-  return r;
+  return r
 }
 
 
@@ -101,8 +93,8 @@ function bq_rewardChoice(lootchestLevel, itemName1, meta1=0, count1=1, itemName2
 /*=============================================
 =           Working with class                =
 =============================================*/
-const questsPath = "../config/betterquesting/DefaultQuests.json";
-var book = new Book(questsPath);
+const questsPath = '../config/betterquesting/DefaultQuests.json'
+var book = new Book(questsPath)
 
 
 /*=============================================
@@ -110,35 +102,35 @@ var book = new Book(questsPath);
 =============================================*/
 
 function replaceReward(quest, newReward) {
-  if (Object.keys(quest["rewards:9"]).length <= 1 && quest["rewards:9"]["0:10"]["rewardID:8"] === "bq_standard:item") {
-    quest["rewards:9"] = newReward;
-    console.log('replaced  :>> ', quest["properties:10"]["betterquesting:10"]["name:8"]);
+  if (Object.keys(quest['rewards:9']).length <= 1 && quest['rewards:9']['0:10']['rewardID:8'] === 'bq_standard:item') {
+    quest['rewards:9'] = newReward
+    console.log('replaced  :>> ', quest['properties:10']['betterquesting:10']['name:8'])
   } else {
-    console.log(' !skipped :>> ', quest["properties:10"]["betterquesting:10"]["name:8"]);
+    console.log(' !skipped :>> ', quest['properties:10']['betterquesting:10']['name:8'])
   }
 }
 
 function replaceRewards(args) {
 
   book.forEachQuestInChapter(args.chapterName, quest => {
-    var id = quest["questID:3"];
+    var id = quest['questID:3']
 
     if        (args.idsByTier[0].includes(id)) {
-      replaceReward(quest, args.rewardByTier[0]);
+      replaceReward(quest, args.rewardByTier[0])
     } else if (args.idsByTier[1].includes(id)) {
-      replaceReward(quest, args.rewardByTier[1]);
+      replaceReward(quest, args.rewardByTier[1])
 
-      quest["preRequisites:11"].forEach(midQuestID => {
-        var midQuestParent = book.questByID(midQuestID);
-        replaceReward(midQuestParent, args.rewardByTier[2]);
-      });
+      quest['preRequisites:11'].forEach(midQuestID => {
+        var midQuestParent = book.questByID(midQuestID)
+        replaceReward(midQuestParent, args.rewardByTier[2])
+      })
     } else {
-      replaceReward(quest, args.rewardByTier[3]);
+      replaceReward(quest, args.rewardByTier[3])
     }
-  });
+  })
 }
 
-/* 
+/*
 
 Replace all rewards in Thaumcraft chapter to choice
 between loot chest and curiocity
@@ -146,40 +138,40 @@ between loot chest and curiocity
 */
 
 replaceRewards({
-  chapterName: "Thaumcraft",
+  chapterName: 'Thaumcraft',
   idsByTier: [
     [654, 673, 679, 675, 680, 674],
     [681, 666, 677, 648, 651, 676, 678, 670, 653, 647],
   ],
   rewardByTier: [
-    bq_rewardChoice(25, "thaumcraft:curio", 4, 1, "thaumcraft:curio", 3, 1),
-    bq_rewardChoice(50, "thaumcraft:curio", 0, 1, "thaumcraft:curio", 2, 1),
-    bq_rewardChoice(75, "thaumcraft:curio", 1, 3),
-    bq_rewardItem("bq_standard:loot_chest", 100),
+    bq_rewardChoice(25, 'thaumcraft:curio', 4, 1, 'thaumcraft:curio', 3, 1),
+    bq_rewardChoice(50, 'thaumcraft:curio', 0, 1, 'thaumcraft:curio', 2, 1),
+    bq_rewardChoice(75, 'thaumcraft:curio', 1, 3),
+    bq_rewardItem('bq_standard:loot_chest', 100),
   ],
-});
+})
 
 
-/* 
+/*
 
 Change rewards for Draconic Evolution quests to better chests
 
 */
 replaceRewards({
-  chapterName: "Draconic Evolution",
+  chapterName: 'Draconic Evolution',
   idsByTier: [
     [546,563,561,553,530],
     [537,7524,534,538,545,556,544],
   ],
   rewardByTier: [
-    bq_rewardItem("bq_standard:loot_chest", 0),
-    bq_rewardItem("bq_standard:loot_chest", 25),
-    bq_rewardItem("bq_standard:loot_chest", 50),
-    bq_rewardItem("bq_standard:loot_chest", 75),
+    bq_rewardItem('bq_standard:loot_chest', 0),
+    bq_rewardItem('bq_standard:loot_chest', 25),
+    bq_rewardItem('bq_standard:loot_chest', 50),
+    bq_rewardItem('bq_standard:loot_chest', 75),
   ],
-});
+})
 
 /*=============================================
 =           Save all changes                  =
 =============================================*/
-utils.saveObjAsJson(book.defaultQuests, questsPath);
+utils.saveObjAsJson(book.defaultQuests, questsPath)
