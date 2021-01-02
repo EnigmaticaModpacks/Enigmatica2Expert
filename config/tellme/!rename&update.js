a4514636e62c9971be48a78da7e3eb8d1ad9daf2
Krutoy242 <Krutoy242@gmail.com>
2021-01-02 16:47:11 +0300

    🧱 Potions-Name-to-id.js refactor

--- config/tellme/!rename&update.js
+++ config/tellme/!rename&update.js
@@ -1,8 +1,8 @@
-const { readdirSync, renameSync } = require('fs');
-const path = require("path");
+const { readdirSync, renameSync } = require('fs')
+const path = require('path')
 
 readdirSync(__dirname)
   .filter(f => f.match(/^.*\.(csv|txt)$/))
   .forEach(f => renameSync(path.resolve(__dirname, f), path.resolve(__dirname, 
-    f.replace(/^([\w-]+?)_.*\.(csv|txt)$/, "$1.$2")
-  )));
+    f.replace(/^([\w-]+?)_.*\.(csv|txt)$/, '$1.$2')
+  )))
--- dev/automation/Potions-Name-to-id.js
+++ dev/automation/Potions-Name-to-id.js
@@ -1,114 +1,89 @@
+/*
 
-const utils = require('./utils.js');
-const csv = require('csvtojson');
+  Potion Names to ID
 
+  This scripts required for megapotion.zs to work
+  There is no way to get Potion ID in game, so
+  static list should be generated outside
 
-function forEachCSVinDir(dirPath, fnc) {
-  utils.readdir(dirPath).forEach(filePath => {
-    if (filePath.split('.').pop() === "csv") {
-      const fileFullPath = dirPath + filePath;
-      fnc(fileFullPath);
-    }
-  });
+  Required command before running
+  /tellme dump-csv potions
+
+*/
+
+const fs = require('fs')
+const {injectInFile, begin, end} = require('../lib/utils.js')
+const csv_parse = require('csv-parse/lib/sync')
+
+begin('  Renaming and updating config/tellme/')
+require('../../config/tellme/!rename&update.js')
+end()
+
+function getCsv(filepath) {
+  return csv_parse(fs.readFileSync(filepath, 'utf8'), {columns: true})
 }
 
-var potionsRegnameTag = [];
+const potionsRegnameTag = getCsv('config/tellme/potions-csv.csv').map(l=>({
+  regName: l['Registry name'],
+  tag: {Id: parseInt(l['ID'])}
+}))
 
-function forEachCSVRow(folder, filenameRegex, fnc, then) {
-  var isFileFound = false;
-  forEachCSVinDir(folder, path => {
-    if (!isFileFound && path.split('/').pop().match(filenameRegex)) {
-      isFileFound = true;
-      var fileContent = utils.loadText(path);
-      csv()
-      .fromString(fileContent)
-      .then((jsonObj)=>{
-        jsonObj.forEach(pot => {
-          fnc(pot);
-        });
-        then();
-      });
+const potions = []
+let elixirNameID = []
+
+getCsv('config/tellme/potiontypes-csv.csv').forEach(pot => {
+  const found = potionsRegnameTag.find(o => {
+    if ( pot.Effects == '') return false
+    const match = pot.Effects.match(/^.*Potion:\[reg:(.*?),.*$/)[1]
+    return o.regName === match
+  })
+
+  if (!found) return
+
+  const normTag = eval(`({${ pot['Effects'].replace(/Potion:\[[^\]]*\],/, '') }})`)
+  const potEffect = normTag.PotionEffect
+
+  // Push potion tags
+  potions.push({
+    name: pot['Registry name'],
+    tag: {
+      Id: found.tag.Id,
+      Amplifier: potEffect.amplifier,
+      Duration : potEffect.duration,
     }
-  });
-}
+  })
 
-var potions = [];
-var elixirNameID = [];
+  // Push Elixir IDs
+  elixirNameID.push({
+    name: found.regName,
+    Id: found.tag.Id,
+  })
+})
 
-forEachCSVRow("../config/tellme/", /^potions-csv.*.csv$/, pot => {
-  potionsRegnameTag.push({regName: pot['Registry name'], tag: {Id: parseInt(pot['ID'])}});
+// Remove duplicates
+elixirNameID = elixirNameID.filter((o, i, self) =>
+  i === self.findIndex(t => (t.name === o.name && t.Id === o.Id))
+)
+elixirNameID.sort((a,b)=>a.Id - b.Id)
 
-}, ()=>{
 
-  forEachCSVRow("../config/tellme/", /^potiontypes-csv.*.csv$/, pot => {
-    var found = potionsRegnameTag.find(o => {
-      if ( pot.Effects == "") return false;
-      var match = pot.Effects.match(/^.*Potion:\[reg:(.*?),.*$/)[1];
-      return o.regName === match;
-    });
+begin(`  Found potions: ${potions.length}, elixirs: ${elixirNameID.length}. Injecting in zs files.`)
 
-    if (found){
-      eval(`var normTag = {${ pot["Effects"].replace(/Potion:\[[^\]]*\],/, "") }}`);
-      var potEffect = normTag.PotionEffect;
+injectInFile('scripts/omegapotion.zs', 
+  'static potNameTag as IData[string] = {\n',
+  '\n} as IData[string];',
+  potions.map(p => {
+    const pad = (member, padStart) => `${member}:${p.tag[member].toString().padStart(padStart)}`
+    return `  "${(p.name+'"').padEnd(40)}: {${pad('Id',3)}, ${pad('Amplifier',3)}, ${pad('Duration',5)}},`
+  }).join('\n')
+)
 
-      // Push potion tags
-      potions.push({
-        name: pot['Registry name'],
-        tag: {
-          Id: found.tag.Id,
-          Amplifier: potEffect.amplifier,
-          Duration : potEffect.duration,
-        }
-      });
+begin('.')
 
-      // Push Elixir IDs
-      elixirNameID.push({
-        name: found.regName,
-        Id: found.tag.Id,
-      });
-    }
-  }, ()=>{
+injectInFile('scripts/omegapotion.zs', 
+  'static elixirNameId as int[string] = {\n',
+  '\n} as int[string];',
+  elixirNameID.map(p => `  "${(p.name+'"').padEnd(40)}: ${p.Id},`).join('\n')
+)
 
-    var scriptFilename = "../scripts/megapotion.zs";
-    var megapotion_zs = utils.loadText(scriptFilename);
-
-    function replaceArray(keyStart, keyFinish, array, makeLineFnc) {
-      var s = keyStart + "\n";
-      array.forEach(p => {
-        s += makeLineFnc(p);
-      });
-      s+= keyFinish;
-  
-      var rgx = new RegExp(utils.escapeRegex(keyStart) + "[\\s\\S\n\r]*?" + utils.escapeRegex(keyFinish), 'm');
-      megapotion_zs = megapotion_zs.replace(rgx, s);
-    }
-
-    replaceArray(
-      "static potNameTag as IData[string] = {",
-      "} as IData[string];",
-      potions,
-      p => {
-        var t = p.tag;
-        var pad = (member, padStart) => `${member}:${t[member].toString().padStart(padStart)}`;
-        return `  "${(p.name+"\"").padEnd(40)}: {${pad("Id",3)}, ${pad("Amplifier",3)}, ${pad("Duration",5)}},\n`;
-      }
-    );
-
-    // Remove duplicates
-    elixirNameID = elixirNameID.filter((o, i, self) =>
-      i === self.findIndex((t) => (t.name === o.name && t.Id === o.Id))
-    )
-
-    replaceArray(
-      "static elixirNameId as int[string] = {",
-      "} as int[string];",
-      elixirNameID,
-      p => {
-        return `  "${(p.name+"\"").padEnd(40)}: ${p.Id},\n`;
-      }
-    );
-
-    utils.saveText(megapotion_zs, scriptFilename);
-  });
-
-});
+end()
