a89d46dc25a52218ec3f916f113dcda1fcbbea0f
Krutoy242 <Krutoy242@gmail.com>
2021-01-02 18:45:24 +0300

    🧱 automate.js - lunch all JS files at once

---
+++ dev/automate.js
@@ -0,0 +1,20 @@
+/*
+
+  Script to automatically evaluate all scripts in
+  dev/automation and other folders
+
+*/
+const glob = require('glob')
+
+const automationList = [
+  ...glob.sync('dev/automation/*.js'),
+  'dev/TCon/tweakerconstruct.js',
+  'dev/Patchouli/Patchouli.js',
+  'scripts/wrap/_wrapper.js'
+]
+
+for(const filePath of automationList) {
+  console.log('📒', filePath)
+  require('.'+filePath.substr(3))
+  console.log('---------------------------\n')
+}
