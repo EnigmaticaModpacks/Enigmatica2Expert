cbe132a618ed7808bf9f4ab9a6c0b5e20c36edc5
Krutoy242 <Krutoy242@gmail.com>
2023-09-17 16:51:33 +0300

    feat(recipes): ✏️[Spectre Leaves] add info that they drop [Ectoplasm]

--- dev/automation/misc.js
+++ dev/automation/misc.js
@@ -174,6 +174,14 @@ export async function init(h = defaultHelper, options = argv) {
     { block_stack: 'forestry:bog_earth:3', dropList: [{ stack: 'forestry:peat', luck: [2, 2] }] },
     { block_stack: 'endreborn:crop_ender_flower', dropList: [{ stack: 'minecraft:ender_pearl' }] },
     { block_stack: 'exnihilocreatio:block_infested_leaves', dropList: [{ stack: 'minecraft:string', luck: [1, 3] }] },
+
+    {
+      block_stack: 'randomthings:spectreleaf',
+      dropList   : [
+        { stack: 'randomthings:spectresapling', luck: [0, 1] },
+        { stack: 'randomthings:ingredient:2' },
+      ],
+    },
   ])
 
   // ###############################################################################
